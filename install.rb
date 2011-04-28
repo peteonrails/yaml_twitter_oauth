5c2dae4c411b5d0cc1d9d9d03eb17923c9bb363d
antonevane <antonevane@gmail.com>
2011-04-28 08:21:39 +0400

    Fixed yml filename

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 puts "Copying the sample Twitter OAuth YML file to your config directory..."
-dest_file = File.join(RAILS_ROOT, 'config', 'twitter_oath.yml')
+dest_file = File.join(RAILS_ROOT, 'config', 'twitter_oauth.yml')
 src_file = File.join(File.dirname(__FILE__) , 'templates', 'twitter_oauth.example.yml')
 FileUtils.cp_r(src_file, dest_file)
 puts "File copied - Installation complete!"
